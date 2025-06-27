78c980fe0dba4d457808dc9b36d3e2eb1f2d22cd
Ian Pichardo <106708916+Greem3@users.noreply.github.com>
2025-06-27 15:07:11 -0400

    feat: Fixed Mine Levels, Farm and Cabin Merged (JustEndy)
    * Fixed Mine Levels, Farm and Cabin Merged
    
    Fixed so only every 10 level it generates a mine channel, and merged the Farmhouse and Cabin so I wont feel so lonely anymore.

--- Discord API/Bot/Methods/ForUsers.cs
+++ Discord API/Bot/Methods/ForUsers.cs
@@ -19,7 +19,17 @@ namespace LVCMod
         /// <returns>Task</returns>
         public async Task MoveToVoice(long playerId, string newLocation)
         {
-            _ = MoveToVoice(GetDiscordFarmerId(playerId), newLocation);
+            _ = MoveToVoice(GetDiscordFarmerId(playerId), MergeLocations(newLocation));
+        }
+
+        private static string MergeLocations(string currentLocation) {
+            if (currentLocation.Contains("UndergroundMine")) {
+                return currentLocation.Substring(0, currentLocation.Length - 1);
+            } else if (currentLocation == "FarmHouse") {
+                return "Cabin";
+            }
+
+            return currentLocation;
         }
 
         public async Task MoveToVoice(ulong playerId, string? newLocation)
